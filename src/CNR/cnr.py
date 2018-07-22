181af7a9ede341551fdfaa36307c477bd8979f58
Houssem El Fekih <cipherd@protonmail.com>
2018-07-23 00:12:51 +0100

    CNR works

--- src/CNR/cnr.py
+++ src/CNR/cnr.py
@@ -7,36 +7,49 @@ from skimage import color,morphology
 import numpy as np
 import pdb
 
-num_clusters = 3
-DEBUG = True
+num_segs = 3
+DEBUG = False
 
 #TODO: Adjust weights with  Xvalidation?
 def CNR(nparr_img,method=2):
   pilimg = PIL.Image.fromarray(nparr_img.astype("float")).convert('RGB')
   im1 = pilimg.filter(ImageFilter.MedianFilter(15))
-  #TODO read adjunct paper and maybe PCA for speed/accuracy?
+
   numpy_img = np.asarray(im1.convert('L'))
   
   p1 = []
-  for row in range(len(numpy_img)):
-    for column in range(len(numpy_img[0])):
+  height = len(numpy_img)
+  width = len(numpy_img[0])
+
+  for row in range(height):
+    for column in range(width):
       if method == 1:
-        p1.append([x*0.15,y*0.15,numpy_img[x][y]])
+        p1.append([row*0.15,column*0.15,numpy_img[row][column]])
       else:
         p1.append([numpy_img[row][column]])
 
-  kmeans = KMeans(init='k-means++', n_clusters=num_clusters)
+  kmeans = KMeans(init=np.array([[np.min(numpy_img)], [int(np.median(numpy_img))],[np.max(numpy_img)]]), n_clusters=num_segs)
+  #kmeans = KMeans(init='k-means++', n_clusters=num_segs)
   kmeans.fit(p1)
 
-  pdb.set_trace()
-  out = kmeans.labels_.reshape((im1.size)[::-1])
+  seg_map = kmeans.labels_.reshape((im1.size)[::-1])
   if DEBUG:
     [_,ax] = plt.subplots(1,3)
-    ax[0].imshow(color.label2rgb(out,numpy_img))
+    ax[0].imshow(color.label2rgb(seg_map,numpy_img))
     ax[1].imshow(sobel(numpy_img))
     ax[2].imshow(numpy_img)
     plt.show()
     pdb.set_trace()
 
-  pdb.set_trace()
-  return 0  
+  regions = [[] for k in range(num_segs)]
+  for row in range(len(numpy_img)):
+    for col in range(len(numpy_img[0])):
+      regions[seg_map[row][col]].append(numpy_img[row][col])
+
+  means = [ np.mean(arr) for arr in regions]
+  bg_noise = np.argmin(means)
+  fg_hard = np.argmax(means)
+  fg_soft = set(range(num_segs)).difference([bg_noise,fg_hard]).pop()
+  CNR = (means[fg_hard] - means[fg_soft]) / means[bg_noise]
+  
+  return CNR  
